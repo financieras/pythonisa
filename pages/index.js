e59eae24913a72cdd8ab86b6f813133393ca3eb9
financieras <financieras@gmail.com>
2022-04-17 12:54:58 +0200

    pagina principal en español

--- pages/index.js
+++ pages/index.js
@@ -13,55 +13,46 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        Finanzas con <a href="https://python.org">Python</a>
+          Finanzas con <a href="https://python.org">Python</a>
         </h1>
-
+        
         <p className={styles.description}>
-          Curso de Python hasta aplicaciones financieras
+          Curso de Python con aplicaciones financieras
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Curso &rarr;</h2>
+            <p>Desde lo básico en Python hasta Objetos y más ...</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Aplicaciones &rarr;</h2>
+            <p>Financieras, actuariales, Web Scraping, bots, ...</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Prácticas &rarr;</h2>
+            <p>Aprender a programar resolviendo ejercicios ...</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Retos &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Nuevos proyectos ... ... ¡Sigue creciendo!
             </p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <small><a href="https://github.com/financieras/pythonisa">Made with ❤ by A. Aparicio • View on Github</a></small>
       </footer>
     </div>
   )
